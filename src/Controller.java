a08c7ac08a178c747dc7c5d408b8159c043bca79
Alexander <alexanderpukhalsky@gmail.com>
2017-03-07 14:49:43 +0300

    Sample example of View-Controller

---
+++ src/Controller.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String page = null;
+		String param = request.getParameter("page");
+		
+		if (param.equals("login")) {
+			page = "/login.jsp";
+		} else if (param.equals("signup")) {
+			page = "/signup.jsp";
+		} else if (param.equals("about")) {
+			page = "/about.jsp";
+		} else {
+			page = "/notfound.jsp";
+		}
+		
+		getServletContext().getRequestDispatcher(page).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
